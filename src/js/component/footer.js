d20e7a5e81c1a61d492d861ef2637e2c44268616
Dilana777 <dylana7792@hotmail.com>
2021-04-29 19:04:08 +0000

    subiendo archivos

---
+++ src/js/component/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Footer() {
+	return (
+		<footer className="page-footer bg-dark">
+			<div className="footer-copyright text-center text-white py-1 pt-5">
+				© 2021 Copyright: Dilana Avendaño
+			</div>
+		</footer>
+	);
+}
